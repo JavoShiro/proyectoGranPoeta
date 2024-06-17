4aba313c3d90d73b7d6279a011ce6669aadddda4
javoshiro <tal_lab@inacap.cl>
2024-06-17 14:33:25 -0400

    Breve actualización a la conexion a la base de datos

--- terminal.py
+++ terminal.py
@@ -1,23 +1,42 @@
-import _mysql_connector 
 import sys
+import mysql.connector
+
+def conectar_mysql():
+    try:
+        conexion = mysql.connector.connect(
+            host=
+            user=
+            password=
+            database=
+        )
+        return conexion
+    except mysql.connector.Error as error:
+        print("Error al conectar a MySQL:", error)
+        sys.exit(1)
 
 def mostrar_menu():
     print("Bienvenido ¿Qué desea?")
     print("1. Iniciar sesión")
     print("2. Salir")
 
-def iniciar_sesion():
+def iniciar_sesion(conexion):
     print("Iniciando sesión...")
 
 def main():
+    conexion = conectar_mysql()
+    cursor = conexion.cursor()
+    
     mostrar_menu()
     opcion = input("Ingrese el número de la opción que desea: ")
     
     if opcion == "1":
-        iniciar_sesion()
+        iniciar_sesion(conexion)
     elif opcion == "2":
         print("Vuelva pronto")
-        sys.exit()
+        sys.exit()  
+    
+    cursor.close()
+    conexion.close()
 
 if __name__ == "__main__":
     main()
