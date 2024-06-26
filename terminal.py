c00064d61cadad8123a81c405a22907a08e1f827
javoshiro <joovierwtf.official@gmail.com>
2024-06-25 21:40:01 -0600

    Se hicieron ajustes al terminal

--- terminal.py
+++ terminal.py
@@ -4,10 +4,10 @@ import mysql.connector
 def conectar_mysql():
     try:
         conexion = mysql.connector.connect(
-            host=
-            user=
-            password=
-            database=
+            host="localhost",      
+            user="root",          
+            password="",          
+            database="elgranpoeta.sql" 
         )
         return conexion
     except mysql.connector.Error as error:
@@ -22,20 +22,38 @@ def mostrar_menu():
 def iniciar_sesion(conexion):
     print("Iniciando sesión...")
 
-def main():
-    conexion = conectar_mysql()
     cursor = conexion.cursor()
+    username = input("Ingrese su nombre de usuario: ")
+    password = input("Ingrese su contraseña: ")
+
+    query = "SELECT * FROM usuarios WHERE nombre_usuario = %s AND contraseña = %s"
+    cursor.execute(query, (username, password))
     
-    mostrar_menu()
-    opcion = input("Ingrese el número de la opción que desea: ")
-    
-    if opcion == "1":
-        iniciar_sesion(conexion)
-    elif opcion == "2":
-        print("Vuelva pronto")
-        sys.exit()  
+    resultado = cursor.fetchone()
+
+    if resultado:
+        print("Inicio de sesión exitoso")
+       
+    else:
+        print("Nombre de usuario o contraseña incorrectos")
     
     cursor.close()
+
+def main():
+    conexion = conectar_mysql()
+    
+    while True:
+        mostrar_menu()
+        opcion = input("Ingrese el número de la opción que desea: ")
+
+        if opcion == "1":
+            iniciar_sesion(conexion)
+        elif opcion == "2":
+            print("Vuelva pronto")
+            break
+        else:
+            print("Opción no válida, por favor intente de nuevo")
+
     conexion.close()
 
 if __name__ == "__main__":
